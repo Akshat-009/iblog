302408feb43b140c6f1d3855dfbf6cc460c87769
akshat <akshat_g@ch.iitr.ac.in>
2020-08-09 22:49:37 +0530

    Added all req html pages

--- blog/urls.py
+++ blog/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("",include('blog.urls')),
+    path("",views.index,name="homepage"),
+    path('about',views.about,name="about"),
+    path('contact',views.contact,name="contact"),
+    path('blog',views.blog,name="blog"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
+def about(request):
+    return render(request,'about.html')
+def contact(request):
+    return render(request,'contact.html')
+def blog(request):
+    return render(request,'blog.html')
