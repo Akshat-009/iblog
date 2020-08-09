b04f548b79a1a9e6ce9baab9df17ce93fc8829bc
akshat <akshat_g@ch.iitr.ac.in>
2020-08-10 00:26:33 +0530

    Made models for posts and

--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,11 @@ class Query(models.Model):
 
     def __str__(self):
         return self.name
+class PostCategory(models.Model):
+    category = models.CharField(max_length=12)
+class Post(models.Model):
+    category=models.ForeignKey(PostCategory,on_delete=models.CASCADE)
+    title = models.CharField(max_length=122)
+    content=models.TextField()
+    timestamp = models.DateTimeField(auto_now=True)
+    slug=models.CharField(max_length=122)
