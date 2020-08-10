7ee07c2a007b1b34ce605b445364b704d0019853
akshat <akshat_g@ch.iitr.ac.in>
2020-08-10 20:44:55 +0530

    Added tinymce editor to the Admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -3,5 +3,9 @@ from .models import Query,PostCategory,Post,BlogComment
 # Register your models here.
 admin.site.register(Query)
 admin.site.register(PostCategory)
-admin.site.register(Post)
+
 admin.site.register(BlogComment)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    class Media:
+        js=('tiny.js',)
